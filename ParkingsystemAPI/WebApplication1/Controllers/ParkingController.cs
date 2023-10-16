80a83fadee9c7960bb89f4857bfcbf5a449719cc
Harm Haandrikman <69154555+NietTeDoen@users.noreply.github.com>
2023-10-16 14:48:32 +0200

    base api + tested
    this api structure worked so now i can work on the base of the database

--- ParkingsystemAPI/WebApplication1/Controllers/ParkingController.cs
+++ ParkingsystemAPI/WebApplication1/Controllers/ParkingController.cs
@@ -2,18 +2,31 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.models;
 
-namespace WebApplication1.Controllers
+namespace ParkingSystemBE.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ParkingController : ControllerBase
     {
+        [HttpGet]
+        [Route("test")]
+        public string APITester()
+        {
+            string successmessage = "your call was successful";
+            return successmessage;
+        }
+        [HttpGet]
+        [Route("get/parkingplaces")]
         public List<ParkingPlace> GetParkingPlaces()
         {
             List<ParkingPlace> ParkingPlaces = new List<ParkingPlace>
             {
-
+            new ParkingPlace { Id = 1, ParkeerPlaats = "A1", Price = 10.00, MaxCarSize = 2, Available = true, Disability = false, Location = "Lot A" },
+            new ParkingPlace { Id = 2, ParkeerPlaats = "B1", Price = 8.50, MaxCarSize = 2, Available = true, Disability = true, Location = "Lot B" },
+            new ParkingPlace { Id = 3, ParkeerPlaats = "C1", Price = 12.00, MaxCarSize = 3, Available = false, Disability = false, Location = "Lot C" },
+            new ParkingPlace { Id = 4, ParkeerPlaats = "D1", Price = 7.50, MaxCarSize = 2, Available = true, Disability = false, Location = "Lot D" }
             };
+
 
             return ParkingPlaces;
         }
