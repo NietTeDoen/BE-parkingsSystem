daa3dff3ef2a97cce58bce9e7fb5186eaf851364
Harm Haandrikman <69154555+NietTeDoen@users.noreply.github.com>
2023-10-16 14:25:59 +0200

    basic api structure
    api structure goes whoooosh

---
+++ ParkingsystemAPI/WebApplication1/Controllers/ParkingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingController : ControllerBase
+    {
+        public List<ParkingPlace> GetParkingPlaces()
+        {
+            List<ParkingPlace> ParkingPlaces = new List<ParkingPlace>
+            {
+
+            };
+
+            return ParkingPlaces;
+        }
+    }
+}
---
+++ ParkingsystemAPI/WebApplication1/models/ParkingPlace.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.models
+{
+    public class ParkingPlace
+    {
+        public int Id { get; set; }
+        public string ParkeerPlaats { get; set; }
+        public double Price { get; set; }
+        public int MaxCarSize { get; set; }
+        public bool Available { get; set; }
+        public bool Disability { get; set; }
+        public string Location { get; set; }
+    }
+}
